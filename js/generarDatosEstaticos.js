fee9d1e41a605bbaa888c3d7ad4dadda0f6b9f1c
yersalcla <yerkoclaros7@gmail.com>
2025-10-24 19:06:38 +0200

    primer ejercicio hecha

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,7 @@
+
+import * as Js1 from "./gestionPresupuestoWeb.js";
+import * as Js2 from "./gestionPresupuesto.js";
+
+
+Js1.mostrarDatoEnId("Hola", "presupuesto");
+    
---
+++ js/gestionPresupuestoWeb.js
@@ -0,0 +1,11 @@
+export function mostrarDatoEnId(valor, id){
+     document.getElementById(id).textContent = valor;
+}
+function mostrarGastoWeb(idElemento,gasto){
+    let id = document.getElementById(idElemento);
+    
+
+}
+function mostrarGastosAgrupadosWeb(){
+
+}
